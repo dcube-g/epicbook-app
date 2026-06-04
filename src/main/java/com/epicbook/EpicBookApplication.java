902bc4c141970c96291cd604c404a815154c246c
root <root@Penguin.localdomain>
2026-06-04 03:08:57 +0000

    Add Spring Boot application

---
+++ src/main/java/com/epicbook/EpicBookApplication.java
@@ -0,0 +1,12 @@
+package com.epicbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpicBookApplication.class, args);
+    }
+}
